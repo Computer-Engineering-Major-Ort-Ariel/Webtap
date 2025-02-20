cf2b7d2af04617bfe85f585418143fe114cd377a
Dvir <dvirarazi@ariel.ort.org.il>
2025-02-20 14:12:43 +0200

    done

--- Program.cs
+++ Program.cs
@@ -15,6 +15,68 @@ class Program
 
     var database = new Database();
 
+    if (!database.Groups.Any())
+    {
+      database.Subjects.Add(new Subject("Web Development", "Dvir Arazi"));
+      database.Subjects.Add(new Subject("C# Fundamentals", "Ofer Shafir"));
+      database.Subjects.Add(new Subject("Physics", "Anna Yelkin"));
+      database.Subjects.Add(new Subject("English", "Liran Roimi"));
+      
+      database.Groups.Add(new Group("Class A"));
+      database.Groups.Add(new Group("Class B"));
+      database.Groups.Add(new Group("Class C"));
+      database.SaveChanges();
+
+      database.Students.Add(new Student("Mika", 1));
+      database.Students.Add(new Student("Dennis", 1));
+      database.Students.Add(new Student("Yuval", 1));
+      database.Students.Add(new Student("Adelina", 2));
+      database.Students.Add(new Student("Yarona", 2));
+      database.Students.Add(new Student("Aviv", 2));
+      database.Students.Add(new Student("Maya G", 2));
+      database.Students.Add(new Student("Maya R", 3));
+      database.Students.Add(new Student("Noam", 3));
+      database.Students.Add(new Student("Nikol", 3));
+      database.SaveChanges();
+
+      database.Grades.Add(new Grade(92, 1, 1));
+      database.Grades.Add(new Grade(95, 1, 3));
+      database.Grades.Add(new Grade(95, 1, 4));
+
+      database.Grades.Add(new Grade(99, 2, 1));
+      database.Grades.Add(new Grade(95, 2, 2));
+
+      database.Grades.Add(new Grade(81, 3, 1));
+      database.Grades.Add(new Grade(85, 3, 2));
+      database.Grades.Add(new Grade(87, 3, 3));
+      database.Grades.Add(new Grade(97, 3, 4));
+
+      database.Grades.Add(new Grade(83, 4, 1));
+      database.Grades.Add(new Grade(92, 4, 3));
+      database.Grades.Add(new Grade(84, 4, 4));
+      
+      database.Grades.Add(new Grade(91, 5, 3));
+
+      database.Grades.Add(new Grade(87, 6, 1));
+      database.Grades.Add(new Grade(80, 6, 1));
+      database.Grades.Add(new Grade(97, 6, 1));
+
+      database.Grades.Add(new Grade(98, 7, 1));
+      database.Grades.Add(new Grade(96, 7, 2));
+      database.Grades.Add(new Grade(84, 7, 4));
+      
+      database.Grades.Add(new Grade(85, 8, 1));
+      database.Grades.Add(new Grade(95, 8, 2));
+      
+      database.Grades.Add(new Grade(87, 9, 2));
+      
+      database.Grades.Add(new Grade(98, 10, 1));
+      database.Grades.Add(new Grade(90, 10, 2));
+      database.Grades.Add(new Grade(89, 10, 3));
+      database.SaveChanges();
+    }
+
+
     while (true)
     {
       (var request, var response) = server.WaitForRequest();
@@ -36,10 +98,31 @@ class Program
       {
         try
         {
-          /*──────────────────────────────────╮
-          │ Handle your custome requests here │
-          ╰──────────────────────────────────*/
-          response.SetStatusCode(405);
+          if (request.Path == "getGroups")
+          {
+            var groups = database.Groups.ToArray();
+
+            response.Send(groups);
+          }
+          if (request.Path == "getStudents")
+          {
+            var groupId = request.GetBody<int>();
+
+            var students = database.Students.Where(student => student.GroupId == groupId);
+
+            response.Send(students);
+          }
+          if (request.Path == "getGrades")
+          {
+            var studentId = request.GetBody<int>();
+            var grades = database.Grades.Where(grade => grade.StudentId == studentId);
+
+            response.Send(grades);
+          }
+          else
+          {
+            response.SetStatusCode(405);
+          }
 
           database.SaveChanges();
         }
@@ -57,14 +140,41 @@ class Program
 
 class Database() : DbBase("database")
 {
-  /*──────────────────────────────╮
-  │ Add your database tables here │
-  ╰──────────────────────────────*/
+  public DbSet<Subject> Subjects { get; set; } = default!;
+  public DbSet<Group> Groups { get; set; } = default!;
+  public DbSet<Student> Students { get; set; } = default!;
+  public DbSet<Grade> Grades { get; set; } = default!;
 }
 
-class User(string id, string username, string password)
+class Group(string name)
 {
-  [Key] public string Id { get; set; } = id;
-  public string Username { get; set; } = username;
-  public string Password { get; set; } = password;
+  [Key] public int Id { get; set; } = default!;
+  public string Name { get; set; } = name;
+}
+
+class Student(string name, int groupId)
+{
+  [Key] public int Id { get; set; } = default!;
+
+  public string Name { get; set; } = name;
+
+  public int GroupId { get; set; } = groupId;
+  [ForeignKey("GroupId")] public Group Group { get; set; } = default!;
+}
+
+class Subject(string name, string teacher)
+{
+  [Key] public int Id { get; set; } = default!;
+  public string Name { get; set; } = name;
+  public string Teacher { get; set; } = teacher;
+}
+
+class Grade(int score, int studentId, int subjectId)
+{
+  [Key] public int Id { get; set; } = default!;
+  public int Score { get; set; } = score;
+  public int SubjectId { get; set; } = subjectId;
+  [ForeignKey("SubjectId")] public Subject Subject { get; set; } = default!;
+  public int StudentId { get; set; } = studentId;
+  [ForeignKey("StudentId")] public Student Student { get; set; } = default!;
 }
---
+++ website/pages/index.ts
@@ -0,0 +1,85 @@
+import { send } from "../utilities";
+
+type Group = {
+    Id: number,
+    Name: string,
+};
+
+type Student = {
+    Id: number,
+    Name: string,
+}
+
+type Subject = {
+    Id: number,
+    Name: string,
+    Teacher: string,
+}
+
+type Grade = {
+    Score: number,
+    Subject: Subject,
+}
+
+let groupSelect = document.querySelector("#groupSelect") as HTMLSelectElement;
+let studentSelect = document.querySelector("#studentSelect") as HTMLSelectElement;
+let gradesTable = document.querySelector("#gradesTable") as HTMLTableElement;
+
+
+async function updateStudentSelect() {
+    let groupId = parseInt(groupSelect.value);
+    let students = await send("getStudents", groupId) as Student[];
+    studentSelect.innerHTML = "";
+    for (let student of students) {
+        let option = document.createElement("option");
+        option.value = student.Id.toString();
+        option.innerText = student.Name;
+        studentSelect.appendChild(option);
+    }
+};
+
+async function updateGradesTable() {
+    let studentId = parseInt(studentSelect.value);
+    let grades = await send("getGrades", studentId) as Grade[];
+    gradesTable.innerHTML = "";
+    for (let grade of grades) {
+        let tr = document.createElement("tr");
+        gradesTable.appendChild(tr);
+
+        let subjectTd = document.createElement("td");
+        tr.appendChild(subjectTd);
+
+        let subjectNameDiv = document.createElement("div");
+        subjectNameDiv.innerText = grade.Subject.Name;
+        subjectTd.appendChild(subjectNameDiv);
+
+        let subjectTeacherDiv = document.createElement("div");
+        subjectTeacherDiv.innerText = grade.Subject.Teacher;
+        subjectTd.appendChild(subjectTeacherDiv);
+
+        let ScoreTd = document.createElement("td");
+        ScoreTd.innerText = grade.Score.toString();
+        tr.appendChild(ScoreTd);
+    }
+}
+
+
+let groups = await send("getGroups", []) as Group[];
+
+for (let group of groups) {
+    let option = document.createElement("option");
+    option.value = group.Id.toString();
+    option.innerText = group.Name;
+    groupSelect.appendChild(option);
+}
+
+await updateStudentSelect();
+
+groupSelect.onchange = async function() {
+    await updateStudentSelect();
+    await updateGradesTable();
+}
+
+await updateGradesTable();
+
+studentSelect.onchange = updateGradesTable;
